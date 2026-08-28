2d924a6431ebf23b04debfacb9d9cb1e2cbde1af
JullioSanntos <JulioSantos@Hotmail.com>
2026-08-28 17:53:27 -0400

    Setup card layout refactored and Setup Template SmartPath control fixed.

---
+++ XpEng.Coder/XpEng.Coder03.Views/Controls/Dimmer.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace XpEng.Coder03.Views.Controls {
+    public static class Dimmer {
+        #region IsActive
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.RegisterAttached(
+                "IsActive", typeof(bool), typeof(Dimmer),
+                new FrameworkPropertyMetadata(true)
+            );
+
+        public static bool GetIsActive(DependencyObject obj) => (bool)obj.GetValue(IsActiveProperty);
+        public static void SetIsActive(DependencyObject obj, bool value) => obj.SetValue(IsActiveProperty, value);
+        #endregion IsActive
+    }
+}
