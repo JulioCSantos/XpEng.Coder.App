c3083902a2b5ad14d87d4a2c81f8ba4be3a823fa
JullioSanntos <JulioSantos@Hotmail.com>
2026-08-28 19:03:06 -0400

    SetupView created with Expander control

--- XpEng.Coder/XpEng.Coder03.Views/Views/PlanItemView.xaml.cs
+++ XpEng.Coder/XpEng.Coder03.Views/Views/PlanItemView.xaml.cs
@@ -1,17 +1,4 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using XpEng.Coder09.Models.Transport;
+﻿using System.Windows.Controls;
 
 namespace XpEng.Coder03.Views.Views
 {
@@ -25,44 +12,5 @@ namespace XpEng.Coder03.Views.Views
             InitializeComponent();
         }
 
-        private void BrowseSetupSolutionFile_Click(object sender, RoutedEventArgs e) {
-            if ((sender as FrameworkElement)?.DataContext is not PlanOrchestratorPoco poco) return;
-
-            var dialog = new OpenFileDialog {
-                Title = "Select Solution File",
-                Filter = "Visual Studio Solution (*.sln;*.slnx)|*.sln;*.slnx|All Files (*.*)|*.*"
-            };
-
-            if (!string.IsNullOrWhiteSpace(poco.SetupSolutionFile)) {
-                var directory = Path.GetDirectoryName(poco.SetupSolutionFile);
-
-                if (!string.IsNullOrWhiteSpace(directory) && Directory.Exists(directory)) {
-                    dialog.InitialDirectory = directory;
-                    dialog.FileName = Path.GetFileName(poco.SetupSolutionFile);
-                }
-            }
-
-            if (dialog.ShowDialog() == true) poco.SetupSolutionFile = dialog.FileName;
-        }
-
-        private void BrowseSetupTemplate_Click(object sender, RoutedEventArgs e) {
-            if ((sender as FrameworkElement)?.DataContext is not PlanOrchestratorPoco poco) return;
-
-            var dialog = new OpenFileDialog {
-                Title = "Select Setup Template File",
-                Filter = "T4 Templates (*.tt)|*.tt|All Files (*.*)|*.*"
-            };
-
-            if (!string.IsNullOrWhiteSpace(poco.SetupTemplatePath)) {
-                var directory = Path.GetDirectoryName(poco.SetupTemplatePath);
-
-                if (!string.IsNullOrWhiteSpace(directory) && Directory.Exists(directory)) {
-                    dialog.InitialDirectory = directory;
-                    dialog.FileName = Path.GetFileName(poco.SetupTemplatePath);
-                }
-            }
-
-            if (dialog.ShowDialog() == true) poco.SetupTemplatePath = dialog.FileName;
-        }
     }
 }
---
+++ XpEng.Coder/XpEng.Coder03.Views/Views/SetupView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using XpEng.Coder09.Models.Transport;
+
+namespace XpEng.Coder03.Views.Views {
+    public partial class SetupView : UserControl {
+        #region constructors
+        public SetupView() {
+            InitializeComponent();
+        }
+        #endregion constructors
+
+        #region methods
+        private void BrowseSetupSolutionFile_Click(object sender, RoutedEventArgs e) {
+            if (DataContext is not PlanOrchestratorPoco poco) return;
+
+            var dialog = new OpenFileDialog {
+                Title = "Select Solution File",
+                Filter = "Visual Studio Solution (*.sln;*.slnx)|*.sln;*.slnx|All Files (*.*)|*.*"
+            };
+
+            SetInitialFile(dialog, poco.SetupSolutionFile);
+            if (dialog.ShowDialog() == true) poco.SetupSolutionFile = dialog.FileName;
+        }
+
+        private void BrowseSetupTemplate_Click(object sender, RoutedEventArgs e) {
+            if (DataContext is not PlanOrchestratorPoco poco) return;
+
+            var dialog = new OpenFileDialog {
+                Title = "Select Setup Template File",
+                Filter = "T4 Templates (*.tt)|*.tt|All Files (*.*)|*.*"
+            };
+
+            SetInitialFile(dialog, poco.SetupTemplatePath);
+            if (dialog.ShowDialog() == true) poco.SetupTemplatePath = dialog.FileName;
+        }
+
+        private static void SetInitialFile(OpenFileDialog dialog, string? filePath) {
+            if (string.IsNullOrWhiteSpace(filePath)) return;
+
+            var directory = Path.GetDirectoryName(filePath);
+            if (string.IsNullOrWhiteSpace(directory) || !Directory.Exists(directory)) return;
+
+            dialog.InitialDirectory = directory;
+            dialog.FileName = Path.GetFileName(filePath);
+        }
+        #endregion methods
+    }
+}
