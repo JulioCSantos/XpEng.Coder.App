8afe4a55f64584da927eaf63bd4c0a460c0e2ece
JullioSanntos <JulioSantos@Hotmail.com>
2026-08-28 23:15:15 -0400

    Source Directories with Exapnder control

--- XpEng.Coder/XpEng.Coder03.Views/Views/SourceDirectoryItemView.xaml.cs
+++ XpEng.Coder/XpEng.Coder03.Views/Views/SourceDirectoryItemView.xaml.cs
@@ -1,32 +1,19 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using XpEng.Coder09.Models.Transport;
 
-namespace XpEng.Coder03.Views.Views
-{
-    /// <summary>
-    /// Interaction logic for SourceDirectoryItemView.xaml
-    /// </summary>
-    public partial class SourceDirectoryItemView : UserControl
-    {
-        public SourceDirectoryItemView()
-        {
+namespace XpEng.Coder03.Views.Views {
+    public partial class SourceDirectoryItemView : UserControl {
+        #region constructors
+        public SourceDirectoryItemView() {
             InitializeComponent();
         }
+        #endregion constructors
 
-
+        #region methods
         private void BrowseSource_Click(object sender, RoutedEventArgs e) {
             if ((sender as FrameworkElement)?.DataContext is not SourceDirectoryPoco poco) return;
 
@@ -36,7 +23,7 @@ namespace XpEng.Coder03.Views.Views
                 path => poco.SourceDirectory = path);
         }
 
-        private void SelectDirectory(string title, string currentPath, Action<string> onFolderSelected) {
+        private static void SelectDirectory(string title, string currentPath, Action<string> onFolderSelected) {
             var dialog = new OpenFolderDialog { Title = title };
 
             if (!string.IsNullOrWhiteSpace(currentPath) && Directory.Exists(currentPath)) {
@@ -45,5 +32,6 @@ namespace XpEng.Coder03.Views.Views
 
             if (dialog.ShowDialog() == true) onFolderSelected(dialog.FolderName);
         }
+        #endregion methods
     }
 }
